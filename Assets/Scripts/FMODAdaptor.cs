d0b1a2849b9010986e1af33522c97c13d76be41e
chavezfk <chavezfk@gmail.com>
2020-08-31 16:25:22 -0600

    working on patches.

---
+++ Assets/Scripts/FMODAdaptor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FMODAdaptor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void callFMODOneShot(string name)
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(name, GetComponent<Transform>().position);
+
+    }
+}
