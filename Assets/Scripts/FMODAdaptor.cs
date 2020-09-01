b7ab1fd7e34bbf838ec0ba2d5928d476a61e8eaa
chavezfk <chavezfk@gmail.com>
2020-08-31 19:55:39 -0600

    98% there.

--- Assets/Scripts/FMODAdaptor.cs
+++ Assets/Scripts/FMODAdaptor.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class FMODAdaptor : MonoBehaviour
 {
+
+    public string eventName;
+
+    private void Start()
+    {
+        if (eventName != "")
+        {
+            FMODUnity.RuntimeManager.PlayOneShot(eventName, GetComponent<Transform>().position);
+        }
+    }
     // Start is called before the first frame update
     public void callFMODOneShot(string name)
     {
